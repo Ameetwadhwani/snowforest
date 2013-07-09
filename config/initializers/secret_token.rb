2e18a9411f0ef94c3346a6ba24fbe46510c8e400
Ameetwadhwani <ameet.wadhwani@gmail.com>
2013-07-09 16:13:29 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snowforest::Application.config.secret_token = 'f272c2f0f7223ba7cdbf3d589a2da4169af59939737400cc8e52c1e46e227f53578af9ec6599ca74676dc76f402b6a9a9bbbe2dee9673ca41a7a920ce5916a50'
