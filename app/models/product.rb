7d8c56892c7300cdc284375b536322b3b72f947e
Ameetwadhwani <ameet.wadhwani@gmail.com>
2013-07-10 13:06:21 -0400

    formatted price from cents to

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ActiveRecord::Base
 	validates :description, :name, :presence => true
 	validates :price_in_cents, :numericality => {:only_integer => true }
 
+
+	def formatted_price
+		price_in_dollars = price_in_cents.to_f / 100
+		f_price = sprintf('%.2f', price_in_dollars)
+		return "$#{f_price}"
+
+	end
+
 end
