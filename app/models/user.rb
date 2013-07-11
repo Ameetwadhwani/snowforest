4d49c040f44451d8af4a518b8c7554fc3483d064
Ameetwadhwani <ameet.wadhwani@gmail.com>
2013-07-11 00:54:57 -0400

    added review model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
 	has_secure_password
-	validates_presence_of :password, :on => :create
+	has_many :reviews
+  has_many :products, :through => :reviews
+  validates_presence_of :password, :on => :create
+
 end
